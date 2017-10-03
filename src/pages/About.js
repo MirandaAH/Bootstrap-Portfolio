3a2de6362892cef738d4ddd9a1a8dd3ea0230115
MirandaAH <hanemiranda@gmail.com>
2017-10-03 02:24:32 -0700

    Image move

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import profilepic from '../Images/profilepic.jpg';
 
 class About extends Component {
   render(){
@@ -10,7 +10,7 @@ class About extends Component {
             <h2 className="maintitle">About Me</h2>
           </div>
           <div className="col-md-12">
-            <img src={'../Images/profilepic.jpg'} id="profile" alt="profile" /><br />
+            <img src={profilepic} id="profile" alt="profile" /><br />
             <p>My name is Miranda Hane. I have been a comedic actress since I was 10 years old. One day, I found a website called CodeAcademy and decided to finally try to learn to code. </p>
             <p>It was pretty daunting at first. I started learning HTML/CSS and struggled in teaching myself how to learn. One day, I remember thinking that it would be impossible to remember everything I was learning and I wasn't ever going to really understand any of it.</p>
             <p>The next day, I sat down at my computer and picked up where I left off the day before. As I sat there, completing the lesson, everything began to click. I started connecting everything I learned and started to truly understand what I was reading and what it meant. I remember jumping out of my chair with joy when I fixed my first bug.</p>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-
+import PsychicGame from "../Images/PsychicGame.png";
+import FriendFinder from "../Images/FriendFinder.png";
+import ComingSoon from '../Images/ComingSoon.png';
 
 class About extends Component {
   render(){
@@ -13,14 +15,14 @@ class About extends Component {
         <div className="row">
           <div className="card-deck">
             <div className="card here">
-              <img className="img-responsive snapshot" src={"../Images/PsychicGame.png"} alt="Psychic Game" />
+              <img className="img-responsive snapshot" src={PsychicGame} alt="Psychic Game" />
               <div className="card-block">
                 <h4 className="card-title title">Are You Psychic?</h4>
                 <a className="card-text" href="https://mirandaah.github.io/Psychic-Game/" target="_blank" rel="noopener noreferrer"><small className="text-muted">View here</small></a>
               </div>
             </div>
             <div className="card here">
-              <img className="img-responsive snapshot" src={"../Images/FriendFinder.png"} alt="Friend Finder" />
+              <img className="img-responsive snapshot" src={FriendFinder} alt="Friend Finder" />
               <div className="card-block">
                 <h4 className="card-title title">Friend Finder</h4>
                 <p className="card-text description">An app that will take a compatibility survey and show you your most compatible friend out of all of members in the database.</p>
@@ -28,7 +30,7 @@ class About extends Component {
               </div>
             </div>
             <div className="card here">
-              <img className="img-responsive" src={'../Images/ComingSoon.png'} alt="Coming Soon" />
+              <img className="img-responsive" src={ComingSoon} alt="Coming Soon" />
               <div className="card-block">
                 <h4 className="card-title title">More Coming Soon</h4>
               </div>
